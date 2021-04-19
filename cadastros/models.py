f74083c8d1f500fdc4e9d8c0caaef9bde0707701
Giovana Thais <giovana.carvalho@ma9.com.br>
2021-04-19 14:31:19 -0300

    feat(atualizando model)

--- cadastros/models.py
+++ cadastros/models.py
@@ -4,41 +4,45 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Cliente(models.Model):
     foto = models.ImageField()
-    nome = models.CharField(max_length=50)
-    sobrenome = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=100)
-    rg = models.CharField(max_length=80)
-    telefone = models.CharField(max_length=12)
-    email = models.EmailField()
+    nome = models.CharField("nome", max_length=50)
+    sobrenome = models.CharField("sobrenome", max_length=100)
+    cpf = models.CharField("cpf", max_length=100)
+    rg = models.CharField("rg", max_length=80)
+    telefone = models.CharField("telefone", max_length=12)
+    email = models.EmailField("email")
     begin_date= models.DateTimeField(auto_now_add=True)
     
     active = models.BooleanField(default=True)
-    
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
     def __str__(self):
-        return str(self.id)
+        return str(self.nome)
 
     class Meta:
-        db_table= 'Cliente'
+        db_table= 'cliente'
+        verbose_name = "Cliente" 
+        verbose_name_plural = "Clientes"
 
 class Endereco(models.Model):
     
-    logradouro = models.CharField(max_length=500)
-    bairro = models.CharField(max_length= 100)
-    cidade = models.CharField(max_length=100)
-    estado = models.CharField(max_length=100)
-    numero = models.CharField(max_length=100)
-    email = models.EmailField()
+    logradouro = models.CharField("logradouro", max_length=500)
+    bairro = models.CharField("bairro", max_length= 100)
+    cidade = models.CharField("cidade", max_length=100)
+    estado = models.CharField("estado", max_length=100)
+    numero = models.CharField("numero", max_length=100)
+    email = models.EmailField("email")
     begin_date= models.DateTimeField(auto_now_add=True)
 
     active = models.BooleanField(default=True)
 
     endereco_principal = models.BooleanField("Endereço principal", default=False)
+    cliente = models.ForeignKey(Cliente,on_delete=models.CASCADE,related_name='endereco_cliente',verbose_name='Cliente')
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     def __str__(self):
         return str(self.id)
 
     class Meta:
-        db_table= 'Endereco'
+        db_table= 'endereco'
+        verbose_name= "Endereço"
+        verbose_name_plural= "Endereços"
 
 # Create your models here.
